3165f1098b785beccc5fac6984a98bb7da2a6949
Poonji <poonjij21@gmail.com>
2025-07-25 19:26:18 +0530

    learnt how to access children of props

---
+++ Intro/project -3/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Card from './components/Card';
+
+function App() {
+  return (
+    <div>
+      <Card name="Poonji">
+      <h1>Poonji is  a good girl</h1>
+      <p> trying so hard to survive</p>
+      <p>she loves a boy who is so cute</p>
+      </Card> 
+    </div>
+  )
+}
+
+export default App
---
+++ Intro/project -3/src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card = (props) => {
+  return (
+    <div>
+       {props.children}{/*to access the 
+       children of the card name*/}
+    </div>
+  )
+}
+
+export default Card
